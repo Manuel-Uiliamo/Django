5edcaef03497c541ec5e7c39ed4946f2230168ac
Manuel Uiliamo <manujw63@gmail.com>
2023-08-09 15:38:24 +0200

    Datbase configuration

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
+
 
 # Create your views here.
-def index(response):
-    return HttpResponse("<h1>Code with Manu!</h1>")
+def index(response, name):
+    ls = ToDoList.objects.get(name=name)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, item.text))
 
 def v1(response):
     return HttpResponse("<h1>View 1!</h1>")
