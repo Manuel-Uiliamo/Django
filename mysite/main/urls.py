559da6a5a19e425790db5c7e593afb070e5f17b8
Manuel Uiliamo <manujw63@gmail.com>
2023-08-14 10:30:39 +0200

    Simple forms

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("<int:id>", views.index, name="index"),
     path("", views.home, name="home"),
+    path("create/", views.create, name="create"),
     path("v1/", views.v1, name="v1"),
 ]
